d75a0a48c52dd36d5d3d1e09482002a47b8892d6
Victor Silva <victorock20@gmail.com>
2022-10-01 23:27:59 -0300

    add random walls after eating food

--- src/constants.py
+++ src/constants.py
@@ -1,28 +1,37 @@
 from enum import Enum
 
 class COLOR:
+    BLUE = (8,115,229)
     LIGHT_BLUE = (56, 212, 218)
     LIGHT_GREY = (200, 200, 200)
     BLACK  = (0, 0, 0)
     WHITE = (255, 255, 255)
-    DARK_GREEN = (0, 100, 0)
     RED = (255, 0, 0)
+    DARKER_GREEN = (74,117,45)
+    DARK_GREEN = (87,138,53)
+    MEDIUM_GREEN = (162,208,74)
+    LIGHT_GREEN = (171,214,83)
 
 class TILE:
     EMPTY = 0
     FRUIT = -1
     WALL = -2
 
+dark = True
+
+def flip_background_color():
+    global dark
+    dark = not dark
 
 def tile_to_color(tile: int) -> tuple:
-    if tile == TILE.EMPTY:
-        return COLOR.WHITE
+    if tile == TILE.EMPTY: 
+        return COLOR.MEDIUM_GREEN if dark else COLOR.LIGHT_GREEN
     elif tile == TILE.WALL:
-        return COLOR.BLACK
+        return COLOR.DARK_GREEN
     elif tile == TILE.FRUIT:
         return COLOR.RED
     elif is_snake(tile):
-        return COLOR.DARK_GREEN
+        return COLOR.BLUE
     else:
         raise ValueError(f"invalid tile value: {tile}")
 
--- src/snake.py
+++ src/snake.py
@@ -1,9 +1,9 @@
 import src.logger as logger
-from src.constants import TILE, COLOR, is_snake, tile_to_color
+from src.constants import TILE, COLOR, tile_to_color, flip_background_color
+from typing import Sequence, Tuple
 import logging as log
 import numpy as np
 import pygame
-
 
 class Game:
     def __init__(self, board_size=32, headless=False):
@@ -14,8 +14,11 @@ class Game:
         self.screen = None
         self.board_size = board_size
         self.board = np.zeros((board_size, board_size), dtype=np.int8)
-        self.window_title = "Snake"
+        self.snake_size = 1
+        self.window_title = "Snake Game"
         self.score = 0
+        self.fps = 10
+        self.clock = None
 
         # make the walls
         self.board[0, :] = TILE.WALL
@@ -24,19 +27,31 @@ class Game:
         self.board[:, board_size - 1] = TILE.WALL
 
         self.board[6, board_size // 2] = 1  # add snake on the board
-        self._add_food()
+        self.__add_food()
 
         self.grid_step = self.width // self.board_size
 
     def run(self):
         """ main game loop """
         self.__initialize()
-        # Event handling
         while self.running:
             for event in pygame.event.get():
                 self.__handle_event(event)
             self.__draw()
+            self.__handle_input()
         pygame.quit()
+
+    def __handle_input(self):
+        pressed_keys = pygame.key.get_pressed()
+        """ handle user input """
+        if pressed_keys[pygame.K_LEFT]:
+            self.__move_snake((-1, 0))
+        elif pressed_keys[pygame.K_RIGHT]:
+            self.__move_snake((1, 0))
+        elif pressed_keys[pygame.K_UP]:
+            self.__move_snake((0, -1))
+        elif pressed_keys[pygame.K_DOWN]:
+            self.__move_snake((0, 1))
 
     def __handle_event(self, event: pygame.event.Event):
         if event.type == pygame.QUIT:
@@ -48,23 +63,22 @@ class Game:
         self.__draw_board()
         self.__draw_score()
         pygame.display.flip()
+        self.clock.tick(self.fps)
 
     def __draw_board(self):
         for x in range(self.board_size):
+            flip_background_color()
             for y in range(self.board_size):
                 x_pos = self.grid_step * x
                 y_pos = self.grid_step * y
+
                 tile_color = tile_to_color(self.board[x, y])
-                
+                flip_background_color()
                 pygame.draw.rect(self.screen, tile_color, [x_pos, y_pos, self.grid_step, self.grid_step])
-                # draw 1px border on the left and top if tile is EMPTY
-                if self.board[x, y] == TILE.EMPTY:
-                    pygame.draw.rect(self.screen, COLOR.LIGHT_GREY, [x_pos, y * self.grid_step, 1, self.grid_step])
-                    pygame.draw.rect(self.screen, COLOR.LIGHT_GREY, [x_pos, y * self.grid_step, self.grid_step, 1])
     
     def __draw_score(self):
         font = pygame.font.Font(None, 30)
-        text = font.render("score: " + str(self.score), 1, COLOR.LIGHT_BLUE)
+        text = font.render("score: " + str(self.score), 1, COLOR.WHITE)
         self.screen.blit(text, (10, 2))
 
     def __initialize(self):
@@ -75,6 +89,7 @@ class Game:
 
         if not self.headless:
             self.screen = pygame.display.set_mode([self.width, self.height])
+            self.clock = pygame.time.Clock()
             pygame.display.set_caption("Snake")
             log.info("screen dimensions set to %s", self.screen.get_size())
         else:
@@ -83,12 +98,39 @@ class Game:
         self.running = True
         log.info("initialization complete")
 
-    def _empty_random_position(self):
+    def __empty_random_position(self):
         """ returns a (x,y) position on the board that is empty ( value = 0 ) """
         empty_positions = np.argwhere(self.board == 0)
         return empty_positions[np.random.randint(0, len(empty_positions))]
 
-    def _add_food(self):
+    def __add_food(self):
         """ adds a food to a random empty position on the board """
-        x, y = self._empty_random_position()
+        x, y = self.__empty_random_position()
         self.board[x, y] = TILE.FRUIT
+    
+    def __add_wall(self):
+        """ adds a wall in a random empty position on the board """
+        x, y = self.__empty_random_position()
+        self.board[x, y] = TILE.WALL
+    
+    def __move_snake(self, direction: Tuple[int, int]):
+        """ moves the snake in the given direction """
+        head_x, head_y = np.argwhere(self.board == 1)[0]
+        self.board[head_x, head_y] = 0
+        x,y = head_x + direction[0], head_y + direction[1]
+        self.__process_board(x,y)
+        self.board[x, y] = 1
+    
+    def __handle_snake_growth(self):
+        """ add a snake_size +1 to the board representing the new snake body"""
+        pass
+
+    
+    def __process_board(self, x, y):
+        """ processes the board and checks for collisions and food """
+        if self.board[x, y] == TILE.FRUIT:
+            self.__add_food()
+            self.__add_wall()
+            self.score += 1
+        elif self.board[x, y] == TILE.WALL:
+            self.running = False
